b53b388fbd1689336fa395ca6daf471df2c3c052
Rajaryan singh <raj.aryan2518@gmail.com>
2025-11-22 18:36:48 +0530

    reverse array

---
+++ b.java
@@ -0,0 +1,36 @@
+public  class b {
+
+    public static int binarySearch(int numbers[], int key){
+
+        int start = 0;
+        int end = numbers.length -1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            if(numbers[mid] == key){
+                return mid;
+            }
+
+            if(numbers[mid] < key){
+                start= mid+1;
+            } else {
+                end = mid-1;
+            }
+
+        }
+
+
+
+
+        return  -1;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 10, 12, 16, 18};
+        int key = 180;
+
+        System.out.println("THE KEY IS AT INDEX: " + binarySearch(numbers, key) );
+
+    }
+}
