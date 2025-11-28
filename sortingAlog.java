fc7e8ee5d78e2448cb50fec2d3eb3e8e3e23839e
Rajaryan singh <raj.aryan2518@gmail.com>
2025-11-28 22:01:28 +0530

    insertion sort

--- sortingAlog.java
+++ sortingAlog.java
@@ -31,6 +31,23 @@ public class sortingAlog {
         }
     }
 
+
+    public static void insertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int curr  = arr[i];
+            int prev = i-1;
+
+            //finding the correct position to insert
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+
+            //insertion
+            arr[prev + 1] = curr;
+        }
+    }
+
     public static void printArr(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print( arr[i] + " ");
@@ -41,7 +58,7 @@ public class sortingAlog {
 
     public static void main(String[] args) {
         int arr[] = {5,3,4, 1,2};
-        selectionSort(arr);
+        insertionSort(arr);
         printArr(arr);
     }
 }
