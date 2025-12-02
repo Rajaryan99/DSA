0fc45fe9ba240d0ad2e0f68c186faef4cb8864a2
Rajaryan singh <raj.aryan2518@gmail.com>
2025-12-02 20:20:01 +0530

    best appraoch

--- 2DArray/diagonalSum.java
+++ 2DArray/diagonalSum.java
@@ -1,20 +1,37 @@
 public class DiagonalSum {
 
-    public static int diagonalSum(int matrix[][]){
+    public static int diagonalSum(int matrix[][]){ //O(n*n)
 
         int sum =0;
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(i == j){
-                    sum += matrix[i][j];
-                }
-                else if( i + j == matrix.length-1){
-                    sum += matrix[i][j];
-                }
-            }
-        }
-        return sum;
+
+        //bruteForce apprach
+    //     for(int i=0; i<matrix.length; i++){
+    //         for(int j=0; j<matrix[0].length; j++){
+    //             if(i == j){
+    //                 sum += matrix[i][j];
+    //             }
+    //             else if( i + j == matrix.length-1){
+    //                 sum += matrix[i][j];
+    //             }
+    //         }
+    //     }
+    //     return sum;
+    // }
+
+
+
+    for(int i=0; i<matrix.length; i++){ //O(n)
+        //pd
+        sum += matrix[i][i];
+
+        //sd
+        if(i != matrix.length-1-i )
+            sum += matrix[i][matrix.length-i-1];
+        
+       
     }
+     return sum;
+}
     public static void main(String[] args) {
         int matrix[][] ={{1,2,3,4},
                          {5,6,7,8,},
