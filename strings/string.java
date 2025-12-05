d478510dcd9cf63dc650cb7e7ea3b9191d1b39de
Rajaryan singh <raj.aryan2518@gmail.com>
2025-12-05 21:06:42 +0530

    printing character in string

--- strings/string.java
+++ strings/string.java
@@ -1,6 +1,12 @@
 import java.util.*;
 
 public class string {
+
+    public static void printChar(String str){
+        for(int i=0; i<str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+    }
     public static void main(String[] args) {
         char arr[] = {'a', 'b', 'c', 'd'};
         String str = "abcd";
@@ -19,7 +25,9 @@ public class string {
         String firstName = "raj";
 
         String fullName = firstName + " " + name;
-        System.out.println(fullName);
+        // System.out.println(fullName);
+
+        printChar(fullName);
 
     }
 }
